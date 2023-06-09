9a5c2a44a0ee31b72c3438d67324c4c0214cea97
kevin george mathew <kevgmat@gmail.com>
2023-06-10 01:47:57 +0530

    fix: reformat file (by penty)

--- susan_skipper/susan_skipper.py
+++ susan_skipper/susan_skipper.py
@@ -7,29 +7,37 @@ import time
 pyautogui.FAILSAFE = True
 pg.PAUSE = 0.25
 
+
 def coordinate(top_left_x, top_left_y, width, height):
-    location = [(top_left_x)/2 + (width)/4,(top_left_y)/2 + (height)/4]
+    location = [(top_left_x) / 2 + (width) / 4, (top_left_y) / 2 + (height) / 4]
     return location
+
 
 hours = int(input("enter number of hours = "))
 interval = 20
-skips = hours * (60/interval)
-skip_time = 20*60
+skips = hours * (60 / interval)
+skip_time = 20 * 60
+
+blocks = skip_time
 print("total skips = ", skips)
 
 time.sleep(3)
 
-
 for i in range(0, int(skips)):
     print("current skip number = ", i)
-    time.sleep(skip_time)
+    # print("|", end = "")
+    for i in range(0, skip_time):
+        if i % 60 == 0:
+            print("|", end="")
+        time.sleep(1)
+    print("")
+    # time.sleep(skip_time)
 
     looper = 0
     page_found = False
     while (page_found == False):
         time.sleep(1)
-        skip = pg.locateOnScreen('skip_light.png', confidence=0.9)
-        print(skip, looper)
+        skip = pg.locateOnScreen('skip_dark.png', confidence=0.9)
         if skip != None:
             page_found = True
             break
@@ -37,25 +45,30 @@ for i in range(0, int(skips)):
             looper += 1
         pg.keyDown("command")
         for i in range(0, looper):
-            print("hello")
             pg.keyDown("tab")
             pg.keyUp("tab")
         pg.keyUp("command")
 
-    skip = pg.locateOnScreen('skip_light.png', confidence = 0.9)
+    skip = pg.locateOnScreen('skip_dark.png', confidence=0.9)
     location = coordinate(skip.left, skip.top, skip.width, skip.height)
-    pg.moveTo(location[0],location[1], duration = 0.25)
+    pg.moveTo(location[0], location[1], duration=0.25)
     pg.click()
 
-    reason = pg.locateOnScreen('reason_dark.png',confidence = 0.9)
-    location = coordinate(reason.left, reason.top,reason.width, reason.height)
-    pg.moveTo(location[0], location[1], duration = 0.25)
+    reason = pg.locateOnScreen('reason_dark.png', confidence=0.9)
+    location = coordinate(reason.left, reason.top, reason.width, reason.height)
+    pg.moveTo(location[0], location[1], duration=0.25)
     pg.click()
 
-    final_skip = pg.locateOnScreen('final_skip_dark.png',confidence = 0.9)
+    final_skip = pg.locateOnScreen('final_skip_dark.png', confidence=0.9)
     location = coordinate(final_skip.left, final_skip.top, final_skip.width, final_skip.height)
-    pg.moveTo(location[0], location[1], duration = 0.25)
+    pg.moveTo(location[0], location[1], duration=0.25)
     pg.click()
+
+    time.sleep(1)
+    pg.keyDown("command")
+    for i in range(0, looper):
+        pg.keyDown("tab")
+        pg.keyUp("tab")
+    pg.keyUp("command")
 
 print("finished skipping")
-
